fee3b01f17fcc92965ea2ea345682a066d713d5b
Yash Raj <yash26raj07@gmail.com>
2026-05-04 12:51:33 +0530

    Update Productinfo.cpp

--- Productinfo.cpp
+++ Productinfo.cpp
@@ -5,7 +5,7 @@ int main(){
      string productName = "Macbook Air M5";
      double productPrice = 199.54;
      float productRating = 4.2;
-     char productCategory = 'E'; // E is being used for electronics 
+     char productCategory = 'E';
        
      cout<<"Product Name : "<<productName<<endl;
      cout<<"Product Price : "<<productPrice<<endl;
